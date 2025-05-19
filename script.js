56fbbb6ef25389a15108bc2cfc2156231d3c8b7d
tse1993 <150841317+tse1993@users.noreply.github.com>
2025-05-19 19:56:38 +0300

    Added Clear button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const screen = document.querySelector('#screen')
-let num1 = ''
-let num2 = ''
+const display = document.querySelector('#screen')
+let firstNum = ''
+let secondNum = ''
 let operator = ''
 
 function add(num1,num2){
@@ -34,3 +34,82 @@ function operate(num1, operator, num2) {
             return divide(num1, num2);
     }
 }
+
+function handleNumberClick(number) {
+  if (operator === '') {
+    firstNum += number;
+  } else {
+    secondNum += number;
+  }
+  updateDisplay();
+}
+
+function handleOperatorClick(op) {
+  if (firstNum !== '') {
+    operator = op;
+    updateDisplay();
+  }
+}
+
+function handleEqualsClick() {
+  if (firstNum !== '' && secondNum !== '' && operator !== '') {
+    const num1 = parseFloat(firstNum);
+    const num2 = parseFloat(secondNum);
+    let result;
+
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 !== 0) {
+          result = num1 / num2;
+        } else {
+          alert("Cannot divide by zero");
+          return;
+        }
+        break;
+      default:
+        return;
+    }
+
+    firstNum = result.toString();
+    secondNum = '';
+    operator = '';
+    updateDisplay();
+  }
+}
+
+
+function updateDisplay() {
+  if (operator === '') {
+    display.textContent = firstNum;
+  } else {
+    display.textContent = `${firstNum} ${operator} ${secondNum}`;
+  }
+}
+
+document.querySelectorAll('.numbers').forEach(button => {
+  button.addEventListener('click', () => handleNumberClick(button.textContent));
+});
+
+document.querySelectorAll('.operators').forEach(button => {
+  button.addEventListener('click', () => handleOperatorClick(button.textContent));
+});
+
+document.querySelector('#equals').addEventListener('click', handleEqualsClick);
+
+document.querySelector('#clearScreen').addEventListener('click', function() {
+    display.textContent = '0';
+    firstNum = ''
+    secondNum = ''
+    operator = ''
+});
+
+
